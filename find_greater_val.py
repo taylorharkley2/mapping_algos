8b881932c07fb9779fa1292f8e6d109d50ab196d
KFair <kaitlin.fair@afacademy.af.mil>
2024-01-08 16:26:48 -0700

    Edited code to return greatest value - works!

---
+++ find_greater_val.py
@@ -0,0 +1,77 @@
+## Last Update: 01/08/2024
+
+## Setup tools
+import matplotlib.pyplot as plt
+import numpy as np
+
+import nengo
+
+from nengo.dists import Uniform
+
+input_vals = np.random.randint(1,7,size=2)
+correct_ans = input_vals.max()
+
+## Create the model object
+model = nengo.Network(label="Addition")
+with model:
+    # Create ensembles each containing 100 leaky integrate-and-fire neurons
+    input_neuron = nengo.Ensemble(500, dimensions=2, radius = 10) 
+    result = nengo.Ensemble(100, dimensions=1, radius = 15)
+
+## Create Inputs
+with model:
+    # Create input node representing an array of constant values
+    input_array = nengo.Node(input_vals)
+
+    # Connect input ensembles to comparator (result)
+    nengo.Connection(input_array,input_neuron)
+
+    # Create a function to return the largest value
+    def compare_neurons(x):
+        if x[0]>x[1]:
+            return x[0]
+        else:
+            return x[1]
+    
+    # Connect input neuron to output neuron (result), train weights to return the highest value
+    nengo.Connection(input_neuron, result, function=compare_neurons)
+
+## Add probes to view data from each ensemble and output
+with model:
+    input_probe = nengo.Probe(input_neuron)
+    result_probe = nengo.Probe(result,synapse=0.01)
+
+# Create the simulator
+with nengo.Simulator(model) as sim:
+    # Run it for 5 seconds
+    sim.run(2)
+
+# set it up so that the plotted ref data is the right dimension
+data_sz = sim.data[result_probe].shape 
+correct_ans_arr = np.repeat(correct_ans,data_sz[0])
+
+## Plot the results!
+# Plot the input signals and decoded ensemble values
+t = sim.trange()
+plt.figure()
+plt.plot(sim.trange(), sim.data[result_probe], label="Neuron Output")
+plt.plot(sim.trange(), correct_ans_arr, label="Right Answer")
+plt.legend()
+plt.ylim(0, 8)
+plt.xlabel("time [s]")
+plt.title("Return Greatest Value")
+plt.show()
+
+# set it up so that the plotted ref data is the right dimension
+input_plot1 = np.repeat(input_vals[0],data_sz[0])
+input_plot2 = np.repeat(input_vals[1],data_sz[0])
+
+plt.figure()
+plt.plot(sim.trange(), sim.data[input_probe], label="Decoded Input Data")
+plt.plot(sim.trange(), input_plot1, label="Input 1")
+plt.plot(sim.trange(), input_plot2, label="Input 2")
+plt.legend()
+plt.ylim(0, 15)
+plt.xlabel("time [s]")
+plt.title("Inputs")
+plt.show()
